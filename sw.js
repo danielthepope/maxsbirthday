365a605daa6f3525dc2443d29f6621ebfee90466
Daniel Pope <danielthepope@users.noreply.github.com>
2016-11-05 00:37:54 +0000

    Added a service worker!

---
+++ sw.js
@@ -0,0 +1,18 @@
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/maxsbirthday/',
+        '/maxsbirthday/index.html',
+        '/maxsbirthday/omgmog.jpg',
+        '/maxsbirthday/omgmog48.png'
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+  );
+});

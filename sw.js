d890194b67b66b15a0da9c69ea7e21b022eb3f10
Daniel Pope <danielthepope@users.noreply.github.com>
2016-11-05 12:58:04 +0000

    Added online fallback

--- sw.js
+++ sw.js
@@ -14,5 +14,8 @@ this.addEventListener('install', function(event) {
 this.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
   );
 });
